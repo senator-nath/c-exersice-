9284beb278d2bb75eb255088f7ba9b9ea47a8072
senator-nath <nathanielosana18@gmail.com>
2023-05-04 09:43:33 +0100

    Add project files.

---
+++ exersice testing/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace exersice_testing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // find the number of upper case characters in "I am On my WaY to SchOol"
+            // char sentence = 'I' 'a''m' 'On' 'my' 'WaY' to SchOol
+            int count = 0;
+            string sentence = "I am On my WaY to SchOol";
+            foreach(char c in sentence)
+            {
+                if (char.IsUpper(c))
+                {
+                    count++;
+                    Console.WriteLine(count);
+                }
+               
+            }
+        }
+
+    }
+}
